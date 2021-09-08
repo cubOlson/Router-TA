150ae0489b6ea685f0ab5c56b5ef522299a1c687
cubOlson <cub.olson@gmail.com>
2021-09-08 11:55:42 -0400

    Refactored with routes

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const margotRouter = require('./routes/margot');
+const margeauxRouter = require('./routes/margeaux');
 
 const app = express();
 
 app.set('view engine', 'pug');
+
+app.use('/margot', margotRouter);
+app.use('/margeaux', margeauxRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello from Express!')
@@ -18,15 +23,7 @@ app.get('/bio', (req, res) => {
     res.send("Bio");
 });
 
-app.get(/\/marg[oteaux]+\/bio/, (req, res) => {
-    res.send("Bio");
-});
-
 app.get('/contact', (req, res) => {
-    res.send("Contact");
-});
-
-app.get(/\/marg[oteaux]+\/contact/, (req, res) => {
     res.send("Contact");
 });
 
@@ -34,11 +31,11 @@ app.get(/.*xyz$/, (req, res) => {
     res.send("That's all I wrote.");
 });
 
-app.get('/about/foo', (req, res) => {
-    res.status(404).send('Not found');
-})
+// app.get('/about/foo', (req, res) => {
+//     res.status(404).send('Not found');
+// })
 
-app.all(/\/\w+/, (req, res) => {
+app.all(/^\/[\w-]+$/, (req, res) => {
     const random = Math.round(Math.random() * 10);
 
     res.render('test', { method: req.method, path: req.path, random })
